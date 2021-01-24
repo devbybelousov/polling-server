84c9fcfab6b31d196e19fbd4b5f1ff518f7a5e57
Dmitry Belousov <devbybelousov@yandex.ru>
2021-01-24 23:21:56 +0700

    Создал базовый проект

---
+++ src/main/java/com/sharespot/pollingserver/PollingServerApplication.java
@@ -0,0 +1,13 @@
+package com.sharespot.pollingserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PollingServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PollingServerApplication.class, args);
+	}
+
+}
